277686ebf176b6c380fd318048026c3b7164d2e3
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-08-28 12:46:12 +0000

    2408280946

--- stackFrame/tryStack.C
+++ stackFrame/tryStack.C
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void try(int a, int b){
+void myfunc(int a, int b){
     // 여기서 %p는 포인터의 주소를 출력한다.
     printf("Inside try, a at %p, b at %p.\n", &a, &b);
     printf("Inside try, a = %d, b = %d.\n", a, b);
     a++;
     b++;
-    printf("Upon increasing, a = %d, b = %d.\n\n". a. b);
-    return
+    printf("Upon increasing, a = %d, b = %d.\n\n", a, b);
+    return;
 }
 
 int main(){
@@ -15,10 +15,15 @@ int main(){
     printf("In main, one at %p, two at %p.\n", &one, &two);
     printf("In main, one = %d, two = %d.\n\n", one, two);
 
-    try(one, two);
+    myfunc(one, two);
 
     printf("Back to main, one at %p, two at %p.\n", &one, &two);
     printf("Back to main, one = %d, two = %d.\n", one, two);
     return 0;
 }
 
+/* 핵심은 main 함수에서의 변수 주소와 myfunc 함수에서의 변수 주소가 다르다는 것이다.
+   "변수 자체가 아니라 변수의 값만 복사해서 넘겨받는다"
+
+   - 그래서 우린 피호출 함수의 매개변수를 '값 매개변수(Value Parameters)' 라고 부른다.
+*/
