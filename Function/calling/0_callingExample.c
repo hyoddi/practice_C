15a1171bea1a52a098b53bab5729ff5a3ff2baa0
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-08-29 03:46:55 +0000

    2408291246

---
+++ Function/calling/0_callingExample.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void increment(int m){
+    m++;
+    printf("Inside function, m is %d.\n", m);
+    return;
+}
+
+int main(){
+    int a = 10;
+    printf("Before calling function, a is %d.\n", a);
+    increment(a);
+    printf("After calling functionm, a is %d.\n", a);
+    return 0;
+}
