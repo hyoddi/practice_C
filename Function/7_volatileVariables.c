c0a96d4b7d5e52ab5e2592e40c9cfba72826a7e3
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-08-29 03:37:35 +0000

    2408291236

--- Function/7_volatileVariables.c
+++ Function/7_volatileVariables.c
@@ -1,1 +1,11 @@
 int ready = 0;
+while (!ready){
+    printf("Wating for the ready signal.\n");
+}
+
+// 이렇게 코드를 작성하게 되면 컴파일러는 ready = 0,
+// 즉, !ready = 1 로 컴파일 할 것이다. 즉 무한 루프를 돌게 될 것.
+// 프로그래머는 외부신호에 의해 ready을 결정하게 할 심산이었겠지만...
+// 이런 뜻하지 않은 일이 일어날 수 있기 때문에 volatile 타입 지정자를 이용해 최적화에서 제외해달라 해야한다.
+
+// volatile : 변동이 있는
