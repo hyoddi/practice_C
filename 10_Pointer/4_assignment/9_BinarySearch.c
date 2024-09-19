5529cd1e4086c52e503427956369f02f1a3e0e38
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-09-19 13:28:31 +0000

    2409192228

--- 10_Pointer/4_assignment/9_BinarySearch.c
+++ 10_Pointer/4_assignment/9_BinarySearch.c
@@ -2,19 +2,32 @@
 // 이진탐색을 이용해서.
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int binary_search(int arr[], int length, int value, int first, int last);
+int binary_search(int arr[], int value, int first, int last){
+    
+    if (first > last) return -1; // 탐색 실패
+    
+    int mid = (first + last) / 2;
+
+    if (arr[mid] == value) return mid;
+    else if (arr[mid] > value) return binary_search(arr, value, first, mid - 1); // 중앙값이 val보다 크다면, 왼쪽 탐색
+    else return binary_search(arr, value, mid + 1, last); // 오른쪽 탐색
+}
 
 int main(){
-    int l = 0, ans = -1;
-    
+
+    int val, arr[12];
+
     printf("Enter twelve integers.\n");
-    while(getchar() != '\n'){
-        arr[l++] = getchar();
+    for (int i = 0; i < 12; i++){
+        scanf("%d", &arr[i]);
     }
+
     printf("Enter data to be searched.\n");
     scanf("%d", &val);
+
+    int ans = binary_search(arr, val, 0, 11);
+
     if (ans != -1) printf("It is in index %d.\n", ans);
-    else printf("No such data.\n")
+    else printf("No such data.\n");
 }
